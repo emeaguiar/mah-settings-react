14dec9d1169a1f63c1c69eafb4669321a86bc355
Mario Aguiar <mario.aguiar@xwp.co>
2024-01-17 12:54:40 -0600

    Parte 3 🚀

--- src/components/settings.js
+++ src/components/settings.js
@@ -7,18 +7,46 @@ import {
     CardBody,
     CardDivider,
     CardFooter,
+    Notice,
+    Spinner,
     TextControl,
     ToggleControl,
 } from '@wordpress/components';
+import { store as coreStore, useEntityProp } from '@wordpress/core-data';
+import { useDispatch } from '@wordpress/data';
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 const Settings = () => {
-    const [ apiKey, setApiKey ] = useState( '' );
-    const [ toggled, setToggled ] = useState( false );
+    const [ isLoading, setIsLoading ] = useState( false );
+    const [ showNotice, setShowNotice ] = useState( false );
+    const [ apiKey, setApiKey ] = useEntityProp(
+        'root',
+        'site',
+        'mah_api_key'
+    );
+    const [ toggled, setToggled ] = useEntityProp(
+        'root',
+        'site',
+        'mah_function'
+    );
+    const { saveEditedEntityRecord } = useDispatch( coreStore );
 
-    const saveSettings = () => {
-        console.log( 'Guardar' );
+    const saveSettings = async () => {
+        setIsLoading( true );
+
+        await saveEditedEntityRecord(
+            'root',
+            'site',
+            undefined,
+            {
+                mah_api_key: apiKey,
+                mah_function: toggled,
+            }
+        );
+
+        setShowNotice( true );
+        setIsLoading( false );
     };
 
     return (
@@ -26,12 +54,24 @@ const Settings = () => {
             <h1>{ __( 'Selecciona las opciones deseadas', 'mah-settings' ) }</h1>
 
             <Card>
+                { showNotice && (
+                    <CardBody>
+                        <Notice
+                            isDismissible
+                            onDismiss={ () => setShowNotice( false ) }
+                            status="success"
+                        >
+                            { __( 'Guardado con éxito', 'mah-settings' ) }
+                        </Notice>
+                    </CardBody>
+                ) }
+
                 <CardBody>
                     <TextControl
                         help={ __( 'Ingresa tu API key', 'mah-settings' ) }
                         label={ __( 'API Key', 'mah-settings' ) }
                         onChange={ setApiKey }
-                        value={ apiKey }
+                        value={ apiKey || '' } 
                     />
                 </CardBody>
 
@@ -41,22 +81,23 @@ const Settings = () => {
                     <ToggleControl
                         label={ __( 'Activar funcionalidad', 'mah-settings' ) }
                         onChange={ setToggled }
-                        checked={ toggled }
+                        checked={ toggled || false }
                     />
                 </CardBody>
 
                 <CardFooter>
                     <Button
-                        disabled={ ! apiKey }
+                        disabled={ ! apiKey || isLoading }
                         onClick={ saveSettings }
                         variant="primary"
                     >
-                        { __( 'Guardar', 'mah-settings' ) }
+                        { isLoading && <Spinner /> }
+                        { ! isLoading && __( 'Guardar', 'mah-settings' ) }
                     </Button>
                 </CardFooter>
             </Card>
         </>
     );
-}
+} 
 
 export default Settings;
